7244f792bf94ecaa9a46dec52a36267c6bdc0c5c
YASH <yashvaghasiya56789@gmail.com>
2025-03-18 19:25:41 +0530

    buy hatavu

--- client/src/pages/home-page.tsx
+++ client/src/pages/home-page.tsx
@@ -10,7 +10,7 @@ export default function HomePage() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-24">
           <div className="text-center">
             <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-              <span className="block">Buy Fresh, Buy Direct</span>
+             
               <span className="block text-primary">from Farmers</span>
             </h1>
             <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
